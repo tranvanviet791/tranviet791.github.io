1c64a42dd0d3b6a732dba9832b071f953a84aa30
tran viet <tranvanviet791@gmail.com>
2017-07-22 18:12:11 +0700

    commit 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3000');
+var socket = io('https://mypeer791.herokuapp.com/');
 $('#div-chat').hide();
 socket.on('danh-sach-online', arrUserInfo => {
     $('#div-chat').show(2000);
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-var io = require('socket.io')(3000);
+var io = require('socket.io')(process.env.PORT || 3000);
 
 const arrUserInfo = [];
 
